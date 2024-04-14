d9927b69cad0a72e0ccecf498033d30fc07774dd
cmolinier <camille.molinier1076@gmail.com>
2024-04-14 11:16:35 +0200

    about section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  openLinkedIn() {
+    const linkedinUrl = 'https://www.linkedin.com/in/camille-molinier/';
+    window.open(linkedinUrl, '_blank');
+  }
+
+  openGitHub() {
+    const githubUrl = 'https://github.com/Camille-Molinier';
+    window.open(githubUrl, '_blank');
+  }
+
+  openKaggle() {
+    const kaggleUrl = 'https://www.kaggle.com/camillemolinier';
+    window.open(kaggleUrl, '_blank');
+  }
 }
