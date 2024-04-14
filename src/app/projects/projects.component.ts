ac6dfcb38cba5a1e14ec97a594775c8530a79fc8
cmolinier <camille.molinier1076@gmail.com>
2024-04-14 22:10:16 +0200

    make projects final list

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 export interface Project {
   title: string;
   description: string;
-  image_path: string; // Use image_path if that's the actual property name
+  image_path: string;
   link: string;
-  badges: string[]; // Assuming badges are represented as an array of strings
+  badges: string[];
 }
 
 @Component({
@@ -18,45 +18,45 @@ export class ProjectsComponent {
     {
       title: 'SSLU',
       description: '2D game with pygame realised in 40h during GameJam ESIR',
-      image_path: 'assets/home.png',
+      image_path: 'assets/images/SSLU.gif',
       link: 'https://github.com/Golden-Sharks/SSLU/tree/main',
       badges: ['Python', 'Pygame']
     },{
       title: 'Sharkio',
       description: 'Distributed system for a multiplayer game',
-      image_path: 'assets/home.png',
-      link: '', //TODO: Add the link to the project
+      image_path: 'assets/images/sharkio.gif',
+      link: 'https://github.com/Sharkio-SR',
       badges: ['SpringBoot', 'Pygame', 'Docker', 'Virtual Machines']
     },{
       title: 'FairPipes',
       description: 'Data mutation pipelines for Machine Learning fairness',
-      image_path: 'assets/home.png',
+      image_path: 'assets/images/fairpipes.png',
       link: 'https://github.com/Camille-Molinier/mutation-testing-ml-fairness',
       badges: ['Machine Learning', 'Python', 'Scikit-Learn']
     },{
       title: 'SSDU',
       description: 'Reinforcement learning framework for web games',
-      image_path: 'assets/home.png',
+      image_path: 'assets/images/ssdu.gif',
       link: 'https://github.com/Golden-Sharks/SSDU',
       badges: ['Reinforcement Learning', 'Python', 'Gym', 'PyQT5']
     },{
       title: 'Untitled Shark Games',
       description: 'Mobile multiplayer game with Android Studio',
-      image_path: 'assets/home.png',
+      image_path: 'assets/images/USG.gif',
       link: 'https://github.com/Golden-Sharks/UntitledSharkGames',
       badges: ['Java', 'Android', 'P2P']
     },{
       title: 'Tower defense',
       description: 'Tower defense game with Java and StdDraw',
-      image_path: 'assets/home.png', // Assuming the image path is correct
+      image_path: 'assets/images/td.gif',
       link: 'https://github.com/Camille-Molinier/Tower_defense',
       badges: ['Java', 'StdDraw']
     },{
       title: 'Python Ascii Art',
-      description: 'Ascii art generator with Python',
-      image_path: 'assets/home.png', // Assuming the image path is correct
-      link: '', //TODO: Add the link to the project
-      badges: ['Python', 'OpenCV']
+      description: 'Ascii art generator with Python and PIL',
+      image_path: 'assets/images/ascii.jpg',
+      link: 'https://github.com/Camille-Molinier/acscii-art-generator',
+      badges: ['Python', 'Pillow']
     },
   ];
 }
